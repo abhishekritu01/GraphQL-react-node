51e55678c6ee7ed8a2857d5b053bc3b436d32a97
abhishek <abhishekritu810219@gmail.com>
2024-04-29 22:46:09 +0530

    test

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { expressMiddleware } = require('@apollo/server/express4');
+const axios = require('axios');
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: `
+        type User {
+            id: ID!
+            name: String!
+            username: String!
+            email: String!
+            phone: String!
+            website: String!
+        }
+        type Todo {
+            id: ID!
+            title: String!
+            completed: Boolean!
+            userId: ID!
+        }
+
+        type Query {
+            getTodo: [Todo]
+            getAllUsers: [User] 
+            getUser(id: ID!): User
+        }
+        `,
+        resolvers: {
+            Query: {
+                // Corrected field name to match schema
+                getTodo: async () => (await axios.get('http://jsonplaceholder.typicode.com/todos')).data,
+                getAllUsers: async () => (await axios.get('http://jsonplaceholder.typicode.com/users')).data,
+                getUser: async (_, { id }) => (await axios.get(`http://jsonplaceholder.typicode.com/users/${id}`)).data
+            }
+        }
+    });
+
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    await server.start();
+
+    app.use('/graphql', expressMiddleware(server));
+
+    app.listen(4000, () => {
+        console.log('Server is running on http://localhost:4000');
+    });
+}
+
+startServer();
